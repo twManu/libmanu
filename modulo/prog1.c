a4980b0e91f9da19ac1f51c0eeeacc4ae418b611
Manu Chen <manuchen@gmail.com>
2016-07-03 22:08:48 +0800

    feat: modulo looks faster ...

--- modulo/prog1.c
+++ modulo/prog1.c
@@ -58,7 +58,7 @@ int main(int argc, char *argv[])
 
 	if( !checkParam(argc, argv) ) exit(-1);
 	for( i=1; i<g_maxValue; ++i ) {
-		if( 1!=(i&0x7) ) continue;       //mod 8 = 1, 4=1
+		if( 1!=(i%8) ) continue;         //mod 8 = 1, 4=1
 		if( i%(7*9) ) continue;          //mod 9 = 0, 3=0, mod 7=0
 		if( 1!=(i%5) ) continue;         //mod 5 = 1
 		if( 3!=(i%6) ) continue;         //mod 6 = 3
