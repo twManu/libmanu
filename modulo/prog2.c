b96e6c8d1f346e39cef653fbcd140635e31c7447
Manu Chen <manuchen@gmail.com>
2016-07-03 22:09:05 +0800

    reorg: as prog1

--- modulo/prog2.c
+++ modulo/prog2.c
@@ -1,38 +1,88 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
+
+#define D4_N            10000
+#define SIZE_DATA	( (sizeof(g_data))/(sizeof(g_data[0])) )
+
+static int g_maxValue = D4_N;                    //maximum to check with
+static int g_verbose = 0;                        //not to show answer
 
 static struct {
 	int modulo;
 	int residule;
 } g_data[] = {
-	  {2, 1}
-	, {3, 0}
-	, {4, 1}
+	  {8, 1}
+	, {63, 0}
 	, {5, 1}
 	, {6, 3}
-	, {7, 0}
-	, {8, 1}
-	, {9, 0}
 };
 
-#define SIZE_DATA	( (sizeof(g_data))/(sizeof(g_data[0])) )
-#define D4_N            10000
+
+static void usage() {
+	printf("Find out values smaller than maxValue with given criteria\n");
+	printf("Usage: prog [-m MAX_VALUE][-v]\n");
+	printf("   MAX_VALUE - default to %d\n", D4_N);
+	printf("   v - verbose answers\n");
+	exit(-1);
+}
+
+
+/*
+ * Check if parameters valid
+ * In  : argc and argv as main
+ * Out : g_MaxValue
+ *       g_verbose
+ * Ret : 1 - successful
+ *       0 - failure
+ */
+static int checkParam(int argc, char *argv[])
+{
+	int opt;
+
+	while( (opt = getopt(argc, argv, "m:vh")) != -1) {
+		switch (opt) {
+		case 'm':
+			g_maxValue = atoi(optarg);
+			break;
+		case 'v':
+			g_verbose = 1;
+			break;
+		default:
+			usage();
+		}
+	}
+
+	if( g_maxValue<=0 ) {
+		printf("Invalid maximal value %d provided\n", g_maxValue);
+		return 0;
+	}
+
+	return 1;
+}
+
+
 
 int main(int argc, char *argv[])
 {
 
-	int i, endN=D4_N;
+	int i, ansCount=0;
 	int loop;
 
 
-	if( argc>1 ) endN=atoi(argv[1]);
-
-	for( i=1; i<endN; ++i ) {
+	if( !checkParam(argc, argv) ) exit(-1);
+	for( i=1; i<g_maxValue; ++i ) {
 		for( loop=0; loop<SIZE_DATA; ++loop )
 			if( g_data[loop].residule != (i%g_data[loop].modulo) )
 				break;
-		if( SIZE_DATA==loop ) printf("%d ", i);
+		if( SIZE_DATA==loop ) {
+			if( g_verbose ) printf("%d ", i);
+			++ansCount;
+		}
 	}
+
+        if( ansCount ) printf("%d answers found\n", ansCount);
+        else printf("Fails to find any answer\n");
 
 	return 0;
 }
