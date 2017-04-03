80a6909a3cdb5b75a65ce80c7341d9bdbe2b8b6e
Manu <manu@manu>
2017-04-03 13:20:35 +0800

    feat: allow file dump

--- v4l2/V4l2BaseTest.cpp
+++ v4l2/V4l2BaseTest.cpp
@@ -1,6 +1,10 @@
-#include "V4l2Base.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <getopt.h>
+#include <fcntl.h>
+#include "V4l2Base.h"
 
 #define BUF_COUNT	4
 
@@ -10,7 +14,7 @@ void usage() {
 	int index;
 	V4l2Base v4l2;
 	printf("Test streaming of given video device\n");
-	printf("Usage: V4l2BaseTest [-n DEVNR] [-c CAP_COUNT] [-f FORMAT] [-s WIDTHxHEIGHT]\n");
+	printf("Usage: V4l2BaseTest [-n DEVNR] [-c CAP_COUNT] [-f FORMAT] [-s WIDTHxHEIGHT] [-d SAVE_FILE]\n");
 	printf("      DEVNR - /dev/videoX which defaults to 0\n");
 	printf("      CAP_COUNT - default to %d\n", g_cap_count);
 	printf("      FORMAT - definded as following\n");
@@ -33,13 +37,20 @@ static int g_devNr = 0;
 static int g_format = -1;
 static int g_width = 640;
 static int g_height = 360;
+static int g_savefd = -1;
+struct header {
+	unsigned int         index;
+	unsigned int         size;
+	unsigned int         data[0];
+} *g_hdr = NULL;
+
 
 bool checkParam(int argc, char *argv[])
 {
 	int opt;
 	int i, w, h;
 
-	while( (opt = getopt(argc, argv, "hn:c:f:s:")) != -1) {
+	while( (opt = getopt(argc, argv, "hn:c:f:s:d:")) != -1) {
 		switch (opt) {
 		case 'n':
 			g_devNr = atoi(optarg);
@@ -58,10 +69,29 @@ bool checkParam(int argc, char *argv[])
 			g_width = w;
 			g_height = h;
 			break;
+		case 'd':
+			g_savefd = open(optarg, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
+			if( g_savefd<0 )
+				perror("open");
+			break;	
 		default:
 			usage();
 		}
 	}
+
+       if( g_savefd>=0 ) {
+                g_hdr = (struct header *)                 //one for index, the other of size
+			calloc(g_cap_count+2, sizeof(unsigned int));
+                if( !g_hdr ) {
+                        close(g_savefd);
+                        printf("failed to allocate header\n");
+                        g_savefd = -1;
+                } else {
+                        g_hdr->index = 0;
+                        g_hdr->size = (g_cap_count+1)*sizeof(unsigned int);
+		}
+	}
+
 	return true;
 }
 
@@ -71,12 +101,13 @@ int main(int argc, char **argv)
 	V4l2Base v4l2;
 	V4l2BaseBuffer *buf;
 	int i, count;
+	unsigned int write_sz;
 
 	checkParam(argc, argv);
 	v4l2.setFormat(g_width, g_height,
 		g_format<0 ? V4L2_PIX_FMT_YUYV : v4l2.enumV4L2Format(g_format));
 	if( !v4l2.initV4l2(g_devNr, BUF_COUNT) ) return -1;
-
+	if( g_savefd>=0 ) lseek(g_savefd, g_hdr->size, SEEK_SET);
 	if( v4l2.streaming(true) ) {
 		for( i=0; i<g_cap_count; ++i ) {
 			buf = v4l2.getBuffer();
@@ -84,11 +115,24 @@ int main(int argc, char **argv)
 				printf("fail to get buffer\n");
 				break;
 			}
-			printf("got buffer[%d], use size=%u\n", buf->getIndex(), buf->GetUsedSize());
+			if( g_savefd>=0 ) {
+				write_sz = write(g_savefd, buf->GetData(), buf->GetUsedSize());
+				printf("write %d(%s) bytes\n", write_sz, write_sz==buf->GetUsedSize()?"OK":"NG");
+				g_hdr->data[g_hdr->index++] = write_sz;   //sample size
+			} else
+				printf("got buffer[%d], use size=%u\n", buf->getIndex(), buf->GetUsedSize());
 			v4l2.putBuffer(buf);
 		}
 		v4l2.streaming(false);
 	} else perror("STREAMON");
+
+	if( g_savefd>=0 ) {
+		unsigned char *tmp = (unsigned char *)g_hdr;
+		tmp += 4;
+		lseek(g_savefd, 0, SEEK_SET);
+		write(g_savefd, tmp, g_hdr->size);
+		close(g_savefd);
+	}
 
 	return 0;
 }
